5fdf18199cfd58b3b6e47e7dd684091244bd83e8
Solomon Birara <solomon.biraraa@gmail.com>
2026-09-16 16:54:04 +0300

    Time: 4 ms (6.74%), Space: 19.2 MB (68.79%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.py
@@ -0,0 +1,7 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        starts={path[0] for path in paths}
+        for path in paths:
+            if path[1] not in starts:
+                return path[1]
+        
